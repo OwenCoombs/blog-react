eafb69c232fb22d42ac9bf4d4455544ed2d3550d
Owen Coombs <owencoombs03@gmail.com>
2024-04-26 08:50:11 -0400

    added contact images

--- src/contact.jsx
+++ src/contact.jsx
@@ -2,6 +2,8 @@ import NavBar from "./navbar";
 import Logo from "./assets/owencircle.png";
 import { Link } from "react-router-dom";
 import linkdin from "./assets/linkedin.svg"
+import email from "./assets/envelope.svg"
+import facebook from "./assets/facebook.svg"
 function Contact(){
     return(
     <div>
@@ -12,14 +14,18 @@ function Contact(){
             <div className="row">
                 <div className="col">
                 <Link to={"https://www.linkedin.com/in/owen-coombs-ab029b254/"} id="linkdinpic">
-                <img src={linkdin}></img>
+                <img src={linkdin} id="linkdin"></img>
                 </Link>
                 </div>
                 <div className="col">
-                    <Link  to={"owencoombs03@gmail.com" }>Email</Link>
+                    <Link  to={"owencoombs03@gmail.com" }>
+                        <img src={email} id="email"></img>
+                    </Link>
                 </div>
                 <div className="col">
-                    <Link to={"https://www.facebook.com/owen.coombs.1441?mibextid=LQQJ4d"}>FaceBook</Link>
+                    <Link to={"https://www.facebook.com/owen.coombs.1441?mibextid=LQQJ4d"}>
+                        <img src={facebook} id="facebook"></img>
+                    </Link>
                 </div>
         </div>
     </div>
